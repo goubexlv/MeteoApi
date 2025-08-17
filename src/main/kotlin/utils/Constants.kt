fa946ff8aa235f3240fd37939ff35718e84c7028
goubexlv <ngoubenejunior@gmail.com>
2025-08-17 20:12:40 +0000

    #update constant

--- src/main/kotlin/utils/Constants.kt
+++ src/main/kotlin/utils/Constants.kt
@@ -5,9 +5,13 @@ import kotlin.String
 
 object Constants {
     val dotenv = dotenv()
-    val VISUALCROSSING_API : String = dotenv["VISUALCROSSING_API"].toString()
-    val HOST_REDIS : String = dotenv["REDIS_HOST"].toString()
-    val PORT_REDIS : Int = dotenv["REDIS_PORT"].toInt()
+//    val VISUALCROSSING_API : String = dotenv["VISUALCROSSING_API"].toString()
+//    val HOST_REDIS : String = dotenv["REDIS_HOST"].toString()
+//    val PORT_REDIS : Int = dotenv["REDIS_PORT"].toInt()
+
+    val VISUALCROSSING_API : String = System.getenv("VISUALCROSSING_API").toString()
+    val HOST_REDIS : String = System.getenv("REDIS_HOST").toString()
+    val PORT_REDIS : Int = System.getenv("REDIS_PORT").toInt()
     const val TIME = 1800L
 
 
